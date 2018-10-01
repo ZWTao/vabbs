f94335aae17833b869790511e754b597bfa05ded
ElevenRob <653668615@qq.com>
2018-10-02 01:08:39 +0800

    Category topic list

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,7 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
+
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
