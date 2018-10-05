103777dbe33ef8f71f95d8032eb499cddb2f9d79
ElevenRob <653668615@qq.com>
2018-10-05 21:38:46 +0800

    AUto translate slug

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use App\Models\Category;
 use Auth;
+use App\Handlers\ImageUploadHandler;
+
 class TopicsController extends Controller
 {
     public function __construct()
@@ -21,8 +23,13 @@ class TopicsController extends Controller
 		return view('topics.index', compact('topics'));
 	}
 
-    public function show(Topic $topic)
+    public function show(Request $request, Topic $topic)
     {
+        // URL 矫正
+        if ( ! empty($topic->slug) && $topic->slug != $request->slug) {
+            return redirect($topic->link(), 301);
+        }
+
         return view('topics.show', compact('topic'));
     }
 
@@ -38,8 +45,8 @@ class TopicsController extends Controller
         $topic->fill($request->all());
         $topic->user_id = Auth::id();
         $topic->save();
-
-        return redirect()->route('topics.show', $topic->id)->with('success', '成功创建话题！');
+        return redirect()->to($topic->link())->with('success', '成功创建话题！');
+//        return redirect()->route('topics.show', $topic->id)->with('success', '成功创建话题！');
 	}
 
 	public function edit(Topic $topic)
@@ -53,8 +60,8 @@ class TopicsController extends Controller
 	{
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
-
-		return redirect()->route('topics.show', $topic->id)->with('success', '更新成功！');
+        return redirect()->to($topic->link())->with('success', '更新成功！');
+//		return redirect()->route('topics.show', $topic->id)->with('success', '更新成功！');
 	}
 
 	public function destroy(Topic $topic)
